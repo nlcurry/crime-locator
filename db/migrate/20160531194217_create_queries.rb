7d7a3f54400b517d652a9d2a813a68d228974f82
Nadine Curry <nlcurry@gmail.com>
2016-05-31 14:00:43 -0700

    started schema and db

--- db/migrate/20160531194217_create_queries.rb
+++ db/migrate/20160531194217_create_queries.rb
@@ -1,6 +1,8 @@
 class CreateQueries < ActiveRecord::Migration
   def change
     create_table :queries do |t|
+      t.float "latitude"
+      t.float "longitude"
 
       t.timestamps null: false
     end
