9a975f96937b9eea98f8f7903fa2b763930b21f0
Nadine Curry <nlcurry@gmail.com>
2016-06-02 13:50:11 -0700

    added create method

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,9 +1,23 @@
 class QueriesController < ApplicationController
-  def index
-
+  def new
+    @query = Queries.new
   end
 
-  def new
+  def create
+    @query = Queries.new(params_access[:query])
 
+    if @query.save
+      redirect_to query_path(@query.id)
+    else
+      render new_query_path
+    end
+  end
+
+
+
+  private
+
+  def params_access
+    params.permit(query: [:address, :latitude, :longitude])
   end
 end
