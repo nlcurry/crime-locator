48c244590f1ba62cc4dbf2b33028d42ef80b99ff
Nadine Curry <nlcurry@gmail.com>
2016-06-05 11:06:00 -0700

    finished show method and changed create method

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,11 +1,12 @@
+require 'HTTParty'
+
 class QueriesController < ApplicationController
   def new
-    @query = Queries.new
+    @query = Query.new
   end
 
   def create
-    @query = Queries.new(params_access[:query])
-
+    @query = Query.create(params_access[:query])
     if @query.save
       redirect_to query_path(@query.id)
     else
@@ -13,6 +14,19 @@ class QueriesController < ApplicationController
     end
   end
 
+  def show
+    @query = Query.find(params[:id])
+    @incidents = HTTParty.get("https://data.seattle.gov/resource/pu5n-trf4.json?$where=within_circle(incident_location, #{@query.latitude.to_s}, #{@query.longitude.to_s}, 5000)")
+    #HTTParty will return an empty array if address is fake or out of Seattle
+    if @incidents.empty?
+        @error = "This address is out of Seattle"
+    else
+        @results = Query.count_incidents(@incidents)
+        @final_results = Query.top_five(@results)
+    end
+    # @results = Query.count_incidents(@incidents)
+    # @final_results = Query.top_five(@results)
+  end
 
 
   private
